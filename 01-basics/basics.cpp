29dbfbb954996e87c3e79196011e63a81d9b571a
Ashma2054 <42852778+Ashma2054@users.noreply.github.com>
2018-09-13 19:04:45 -0500

    Update basics.cpp

--- 01-basics/basics.cpp
+++ 01-basics/basics.cpp
@@ -5,8 +5,13 @@ int sum_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
-//write code for multiply_numbers function here
+
 int multiply_numbers(int num1, int num2)
 {
 	return num1 * num2;
+}
+void pass_by_val_by_ref(int val, int &ref)
+{
+	val = 99;
+	ref = 99;
 }
