2201e5285d13f1508b78d22ead62cec053f0cc42
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-10-12 16:08:53 -0500

    Test case names must be unique

--- homeworks/04-vectors-test/vectors_test.cpp
+++ homeworks/04-vectors-test/vectors_test.cpp
@@ -96,7 +96,7 @@ TEST_CASE("Test vector of primes")
 	
 }
 
-TEST_CASE("Test vector of primes")
+TEST_CASE("Test vector of primes 50")
 {
 	vector<int> primes_up_to_50{ 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47 };
 
